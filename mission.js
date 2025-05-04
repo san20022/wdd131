49ff95a393931f7a13f0f91beb4ff99d7c034c02
san20022 <san20022@byui.edu>
2025-05-04 16:59:33 -0600

    Update

---
+++ mission.js
@@ -0,0 +1,16 @@
+const themeSelector = document.querySelector('#theme-selector');
+
+function changeTheme() {
+    const body = document.body;
+    const logo = document.querySelector('.logo');
+
+    if (themeSelector.value === 'dark') {
+        body.classList.add('dark');
+        logo.src = 'byui-logo-white.png';
+    } else {
+        body.classList.remove('dark');
+        logo.src = 'byui-logo_blue.webp';
+    }
+}
+
+themeSelector.addEventListener('change', changeTheme);
